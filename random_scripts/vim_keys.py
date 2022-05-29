97fb01391917bbbf974dc7e110f4b5a0d485186e
carloslvm <cvaldez553@gmail.com>
2022-05-29 17:01:38 -0400

    Print Vim Keybindings

---
+++ random_scripts/vim_keys.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This script reads my personal vimrc file and filter my
+custome keybindings. As preference, I put this script
+in the same directory where the vimrc file is located.
+
+If you want to save this in a different directory, make
+sure to edit the path in the source code of this script
+and vim_shortcuts.py script.
+
+Author: Carlos
+"""
+# File to read
+vimrc = '/home/carlos/.vim/vimrc'
+
+# Reading and creating a list of each line in vimrc.
+with open(vimrc, 'r') as vim:
+    vim_shortcuts = vim.readlines()
+
+# Printing the selected lines from the previous list.
+for key in vim_shortcuts[58:]:
+    print(key)
---
+++ random_scripts/vim_shortcuts.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+This script is connected to vim_keys.py script. It allows
+to show my personal Vim keybindings in a neatly format
+using dmenu.
+
+It required to have dmenu installed on your Linux distro
+to make it work.
+
+Author: Carlos
+"""
+import os
+
+# dmenu options
+text_color = "\"#BD9842\" -sb "
+selector = "\"#4F3600\" -sf "
+f_selector = "\"#FFCD59\" -b -l 20"
+prompt = "\"Vim Shortcuts\" -nf "
+menu = "dmenu -p "
+
+# Command Variable
+dmenu = menu + prompt + text_color + selector + f_selector
+
+# Printing Vim keybindings
+vim_keybind = "python3 /home/carlos/.vim/vim_keys.py"
+
+# Cleaning output (removing useless spaces)
+space = " | sed '/^$/d' "
+
+# Full Command
+command = vim_keybind + space + "| "  + dmenu
+
+# Run the Command
+os.system(command)
